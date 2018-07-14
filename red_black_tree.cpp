5b226e387b4ed05740914aaf8b49d37dd32c10c0
Grigor <grigortovmasian@gmail.com>
2018-07-14 22:29:28 +0400

    Add Red Black Tree class
    Functions will be implemented later.
    
    Signed-off-by: Grigor <grigortovmasian@gmail.com>

---
+++ red_black_tree.cpp
@@ -0,0 +1,17 @@
+#include "red_black_tree.h"
+
+CRedBlackTree::CRedBlackTree():CBinaryTree() {}
+
+CRedBlackTree::~CRedBlackTree() {}
+
+CNode * CRedBlackTree::insert(int value) {
+
+}
+
+CNode * CRedBlackTree::remove(int value) {
+
+}
+
+void CRedBlackTree::balance(CNode * node) {
+
+}
--- red_black_tree.h
+++ red_black_tree.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "binary_tree.h"
+
+class CRedBlackTree : public CBinaryTree {
+public:
+	CRedBlackTree();
+	~CRedBlackTree();
+	virtual CNode * insert(int);
+	virtual CNode * remove(int);
+protected:
+	virtual void balance(CNode *);
+};
