2aae7a1bcf0f54b04f1f05e99e11a8e29fdbb267
Grigor <grigortovmasian@gmail.com>
2018-07-14 21:57:31 +0400

    Add binary search tree class
    Function bodies will be added later.:w
    
    Signed-off-by: Grigor <grigortovmasian@gmail.com>

---
+++ binary_tree.cpp
@@ -0,0 +1,46 @@
+#include "binary_tree.h"
+
+CBinaryTree::CBinaryTree():m_root(nullptr) {}
+
+CBinaryTree::~CBinaryTree() {
+
+}
+
+CNode * CBinaryTree::insert(int value) {
+
+}
+
+CNode * CBinaryTree::find(int value) {
+
+}
+
+CNode * CBinaryTree::remove(int value) {
+
+}
+void CBinaryTree::print_inorder() {
+
+}
+
+void CBinaryTree::print_postorder() {
+
+}
+
+void CBinaryTree::print_preorder() {
+
+}
+
+void CBinaryTree::left_rotate(CNode * node) {
+
+}
+
+void CBinaryTree::right_rotate(CNode * node) {
+
+}
+
+void CBinaryTree::balance(CNode *) {
+
+}
+
+void CBinaryTree::delete_recursive_helper() {
+
+}
--- binary_tree.h
+++ binary_tree.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "node.h"
+
+class CBinaryTree {
+public:
+	CBinaryTree();
+	virtual ~CBinaryTree();
+	virtual CNode * insert(int value);
+	CNode * find(int value);
+	virtual CNode * remove(int value);
+	void print_inorder();
+	void print_preorder();
+	void print_postorder();
+protected:
+	CNode * m_root;
+	/* functions only for inherit clases */
+	void left_rotate(CNode *);
+	void right_rotate(CNode *);
+	virtual void balance(CNode *);
+private:
+	void delete_recursive_helper();
+};
