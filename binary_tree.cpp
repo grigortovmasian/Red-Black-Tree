96163d8c1bf6b7664630ee40e9d7661309313774
Grigor <grigortovmasian@gmail.com>
2018-07-15 15:50:01 +0400

    Add find and insert functions for binary tree
    Signed-off-by: Grigor <grigortovmasian@gmail.com>

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -7,11 +7,58 @@ CBinaryTree::~CBinaryTree() {
 }
 
 CNode * CBinaryTree::insert(int value) {
+	CNode * new_node = new CNode();
+	new_node->set_value(value);
+
+	if (m_root == nullptr) {
+		m_root = new_node;
+		return new_node;
+	}
+
+	CNode * iter = m_root;
+	while (true)
+	{
+		if (iter->get_value() == value)
+			return nullptr;
+
+		if (iter->get_value() > value)
+			if (iter->get_left())
+				iter = iter->get_left();
+			else {
+				new_node->set_parent(iter);
+				iter->set_left(new_node);
+				return new_node;
+			}
+		else
+			if (iter->get_right())
+				iter = iter->get_right();
+			else {
+				new_node->set_parent(iter);
+				iter->set_right(new_node);
+				return new_node;
+			}
+	}
+
+
 
 }
 
 CNode * CBinaryTree::find(int value) {
 
+	CNode * ret = m_root;
+
+	while (ret)
+	{
+		if (ret->get_value() == value)
+			return ret;
+
+		if (ret->get_value() > value)
+			ret = ret->get_left();
+		else
+			ret = ret->get_right();
+	}
+
+	return ret;
 }
 
 CNode * CBinaryTree::remove(int value) {
